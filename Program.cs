261a626cc26e6e752ff4870196de5f4fefe7150a
edfranco2021 <edgardofranco@gmail.com>
2022-03-11 22:05:04 -0300

    se agrega el programa para sumar

--- Program.cs
+++ Program.cs
@@ -18,3 +18,26 @@
     } //fin Main
 }
 
+/* programa para sumar 
+using System;
+
+namespace contador
+{ 
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      int contador = 0, suma = 0;
+      while(contador < 10)
+      {
+        suma += contador;   //igual a suma = suma + contador
+        contador++;   //igual a contador + 1
+        Console.WriteLine(suma);
+      }
+
+      Console.WriteLine("La suma final es: " + suma);
+    }
+  }
+}
+*/
+
